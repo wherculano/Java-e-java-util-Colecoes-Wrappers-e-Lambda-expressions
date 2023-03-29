3ed8b33615f8e7dc7bac769f544785af032bfe84
wagner <wagner.santos@accurate.com.br>
2023-03-29 15:15:46 -0300

    Introducao ao ArrayList

---
+++ aula_001_arrays/src/curso/java/alura/test/util/Teste.java
@@ -0,0 +1,47 @@
+package curso.java.alura.test.util;
+
+import curso.java.alura.modelo.Conta;
+import curso.java.alura.modelo.ContaCorrente;
+
+import java.util.ArrayList;
+
+public class Teste {
+    public static void main(String[] args) {
+        ArrayList lista = new ArrayList();
+
+        Conta cc1 = new ContaCorrente(11, 22);
+        lista.add(cc1);
+
+        Conta cc2 = new ContaCorrente(33, 44);
+        lista.add(cc2);
+
+        System.out.println("Tamanho da lista: " + lista.size());
+
+        Conta conta = (Conta) lista.get(0);
+        System.out.println(conta);
+        System.out.println(conta.getNumero());
+
+        lista.remove(1);
+        System.out.println("Tamanho da lista: " + lista.size());
+
+        Conta cc3 = new ContaCorrente(44, 555);
+        lista.add(cc3);
+
+        Conta cc4 = new ContaCorrente(66, 777);
+        lista.add(cc4);
+
+        System.out.println("-".repeat(40));
+
+        for (int i=0; i < lista.size(); i++){
+            Object objRef = lista.get(i);
+            System.out.println(objRef);
+        }
+
+        System.out.println("-".repeat(40));
+
+        for(Object obj : lista){
+            System.out.println(obj);
+        }
+
+    }
+}
