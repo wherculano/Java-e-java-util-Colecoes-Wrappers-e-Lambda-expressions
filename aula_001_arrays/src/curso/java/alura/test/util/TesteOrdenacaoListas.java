b7cdbc745ea7f7c1c6237c036f13554c4681ecaf
wagner <wagner.santos@accurate.com.br>
2023-03-30 16:25:07 -0300

    Ordenando listas tambem por nomes

--- aula_001_arrays/src/curso/java/alura/test/util/TesteOrdenacaoListas.java
+++ aula_001_arrays/src/curso/java/alura/test/util/TesteOrdenacaoListas.java
@@ -1,6 +1,7 @@
 package curso.java.alura.test.util;
 
 
+import curso.java.alura.modelo.Cliente;
 import curso.java.alura.modelo.Conta;
 import curso.java.alura.modelo.ContaCorrente;
 import curso.java.alura.modelo.ContaPoupanca;
@@ -14,16 +15,29 @@ public class TesteOrdenacaoListas {
     public static void main(String[] args) {
 
         Conta cc1 = new ContaCorrente(22, 33);
+        Cliente clienteCC1 = new Cliente();
+        clienteCC1.setNome("Wagner");
+        cc1.setTitular(clienteCC1);
         cc1.deposita(333.0);
 
         Conta cc2 = new ContaPoupanca(22, 44);
+        Cliente clienteCC2 = new Cliente();
+        clienteCC2.setNome("Matheus");
+        cc2.setTitular(clienteCC2);
         cc2.deposita(444.0);
 
         Conta cc3 = new ContaCorrente(22, 11);
+        Cliente clienteCC3 = new Cliente();
+        clienteCC3.setNome("Danielle");
+        cc3.setTitular(clienteCC3);
         cc3.deposita(111.0);
 
         Conta cc4 = new ContaPoupanca(22, 22);
+        Cliente clienteCC4 = new Cliente();
+        clienteCC4.setNome("Milena");
+        cc4.setTitular(clienteCC4);
         cc4.deposita(222.0);
+
 
         List<Conta> lista = new ArrayList<>();
         lista.add(cc1);
@@ -31,18 +45,31 @@ public class TesteOrdenacaoListas {
         lista.add(cc3);
         lista.add(cc4);
 
+        System.out.println("-".repeat(16) + " LISTA SEM ORDENACAO " + "-".repeat(16));
         for (Conta conta : lista) {
-            System.out.println(conta);
+            System.out.println(conta + " -> " + conta.getTitular().getNome());
         }
 
-        System.out.println("-=".repeat(20));
         NumeroDaContaComparator comparator = new NumeroDaContaComparator();
-        lista.sort(comparator);
 
+        System.out.println("-".repeat(13) + " LISTA ORDENADA PELO NUMERO " + "-".repeat(13));
+
+        lista.sort(comparator);
         for (Conta conta : lista) {
-            System.out.println(conta);
+            System.out.println(conta + " -> " + conta.getTitular().getNome());
         }
+
+        System.out.println("-".repeat(14) + " LISTA ORDENADA PELO NOME " + "-".repeat(14));
+
+        TitularDaContaComparator titularComparator = new TitularDaContaComparator();
+
+        lista.sort(titularComparator);
+        for (Conta conta : lista) {
+            System.out.println(conta + " -> " + conta.getTitular().getNome());
+        }
+
     }
+
 }
 
 class NumeroDaContaComparator implements Comparator<Conta> {
@@ -54,5 +81,14 @@ class NumeroDaContaComparator implements Comparator<Conta> {
             return 1;   // Pode retornar qualquer numero positivo.
         }
         return 0;
+    }
+}
+
+class TitularDaContaComparator implements Comparator<Conta> {
+    @Override
+    public int compare(Conta c1, Conta c2) {
+        String nomeClienteC1 = c1.getTitular().getNome();
+        String nomeClienteC2 = c2.getTitular().getNome();
+        return nomeClienteC1.compareTo(nomeClienteC2);
     }
 }
